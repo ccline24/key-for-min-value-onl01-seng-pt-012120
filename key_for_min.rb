b70b0c6dae0fecc1fae505bc73d625f2c73985b5
Chance cline <clinechance@gmail.com>
2020-02-04 04:16:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 require "pry"
+
 def key_for_min_value(name_hash)
+  smallest_name = nil
+  smallest_num = nil
  name_hash.each do |name, num|
-   
+   if smallest_num == nil || num < smallest_num
+     smallest_num = num 
+  
      binding.pry
 end
 end
